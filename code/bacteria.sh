49881cc8b889414f725fa2a5df1db358cb48057f
PacchaGloriagiotech <gloria.paccha@est.ikiam.edu.ec>
2022-11-30 02:09:12 -0500

    Paccha Gloria ha contado las bacterias

--- code/bacteria.sh
+++ code/bacteria.sh
@@ -1,11 +1,11 @@
 #contar el numero de cada bacteria
-tail -n +2 ../data/grampa.csv | cut -d "," -f 2 | sort | uniq -c >> ../data/bacteria.csv
+tail -n +2 ../data/grampa.csv | cut -d "," -f 2 | sort | uniq -c >> ../data/bacteria.csv 
 # el valor mas alto de bacteria
 echo "especie  mas alto" >> ../data/bacteria.csv
 tail -n +2 ../data/grampa.csv | cut -d "," -f 2 | grep "E. coli" | sort | uniq -c >> ../data/bacteria.csv
 #valor mas bajo de bacteria
 echo "especie  mas baja" >> ../data/bacteria.csv
 tail -n +2 ../data/grampa.csv | cut -d "," -f 2 | grep "V. toreilis" | sort | uniq -c >> ../data/bacteria.csv 
-  #
+ #Ejercicio 4
 
 
